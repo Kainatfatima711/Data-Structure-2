7be661281c05af6e2c37302ece62e487c58dd63f
Kainatfatima711 <kainatfetima@gmail.com>
2026-01-18 14:17:39 +0500

    Add files via upload

---
+++ ImplementBitwise.py
@@ -0,0 +1,12 @@
+#Program to showcase working of different types of bitwise operators
+num1 = 0
+num2 = 4
+
+#Using AND operator
+print("num1 & num2 = " , num1 & num2)
+
+#Using Not operator
+print("\n~num1 = ", ~num1)
+
+#Usinf XOR operator
+print("\n num1 ^ num2 = " , num1 ^ num2)
